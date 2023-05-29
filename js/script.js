54a2049cb696d34e852e51837582f7711cebeeaf
Tiago Dalmolin <tiagodalmolin028@gmail.com>
2023-05-29 00:33:56 -0300

    final

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 //toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+
 
 
 //scroll sections
@@ -19,10 +28,27 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*= ' + id + ']').classList.add('active');
             });
+            //active sections for animation on scroll
+            sec.classList.add('show-animate');
         }
+        //if want to use animation that repeats on scroll use this
+        else{                          
+        sec.classList.remove('show-animate');
+    }
     });
     // sticky header
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    //remove toggle icon and navbar when click navbar links(scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+    //Animation footer on scroll
+    let footer = document.querySelector('footer');
+
+
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.
+    scrollHeight);
 }
